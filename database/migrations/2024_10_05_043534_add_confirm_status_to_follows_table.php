58bdcace7fdf3a429de78099badc2a8a54376377
Zeus-M <ahmadmohib127@gmail.com>
2024-10-14 16:25:33 +0500

    Test Commit

--- database/migrations/2024_10_05_043534_add_confirm_status_to_follows_table.php
+++ database/migrations/2024_10_05_043534_add_confirm_status_to_follows_table.php
@@ -28,3 +28,28 @@ return new class extends Migration
         });
     }
 };
+
+/*
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     
+    public function up()
+    {
+        Schema::table('follows', function (Blueprint $table) {
+            $table->unsignedTinyInteger('confirm_status')->default(0);  
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('follows', function (Blueprint $table) {
+            $table->dropColumn('confirm_status'); 
+        });
+    }
+};*/
